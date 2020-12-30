7cd47463ca2da45f77097414be8ee80bba91f7ba
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2020-12-30 21:22:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gradienteffect/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.gradienteffect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button log,sign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        log=findViewById(R.id.btnLog);
+        sign=findViewById(R.id.btnSign);
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this,  "Log In", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this , "Sign Up", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
+
+
